820416d09e36acc2ab6ba25df5a7998b4d0f3d56
silvioukoth <silvioukothh@gmail.com>
2023-03-23 00:57:39 +0000

    add correction

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-function getFullResponseFrom API(success) {
+function getFullResponseFromAPI(success) {
   return new Promise(resolve, reject) => {
     if (success) resolve({status: 200, body: 'Success'});
     reject(Error('The fake API is not working currently'))
