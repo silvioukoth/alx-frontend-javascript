c88d6747740913b690acb20425b63744e211da5f
silvioukoth <silvioukoth@gmail.com>
2023-03-31 11:41:47 +0000

    add update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return [
-    {id: 1, firtName: 'Guilaume', location: 'San francisco' },
-    {id: 2, firstName: 'James', location: 'Columbia' },
-    {id: 5, firstName: 'Serena', location: 'San francisco' },
+    { id: 1, firtName: 'Guilaume', location: 'San francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San francisco' },
   ];
 }
